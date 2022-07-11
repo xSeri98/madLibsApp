5bd1056e11e94a1a17152eaf5aa3ca9bf50125c5
xSeri98 <ronquillo.236225@pasay.edsa.sti.edu.ph>
2022-07-11 08:06:44 +0800

    Finished Project

--- app.js
+++ app.js
@@ -15,24 +15,25 @@ let questionCounter = 0;
 
 
 for(let i = numberOfQuestion; i>0; i--){
-questionArray.push(prompt(` You still have to replace : ${i} words, Enter a verb", "Enter a noun`));
+userInputs.push(questionArray[questionCounter] = prompt(` You still have to replace : ${i} words, Enter a verb", "Enter a noun`));
 console.log(questionArray[questionCounter] + `; (${i-1} questions left)`);
 questionCounter++;
 }
+let modifiedStory = `<h2 id="SprintProject"><mark>Once upon a time, in a land far away, there lived a nameless ${userInputs[0]}. The monster was dying to have a ${userInputs[1]}. So he decided to set out on a journey to find his ${userInputs[2]}.</h2>
+<h2><mark>But the world was a large ${userInputs[3]}, so the monster split into two on his journey. One monster went to the ${userInputs[4]}. And the other went to the ${userInputs[5]}. The monster who went ${userInputs[6]} came to a village. "Mr. Blacksmith, please give me your ${userInputs[7]}."</h2>
+<h2><mark>"You can't give someone a ${userInputs[8]}."
+"If you give me your ${userInputs[9]}, I'll jump into your stomach and make you ${userInputs[10]} in return."</h2>
+<h2><mark>"Really? You'll make me ${userInputs[11]}? Okay, you can have my ${userInputs[12]}."
+And the monster went inside the blacksmith's ${userInputs[13]}.</h2>
+<h2><mark>The monster became Otto the blacksmith. Otto was the ${userInputs[14]} man in town. But one day, "Look at me, look at me. The ${userInputs[15]} inside me has grown this large."</h2>
+<h2><mark>Munch-munch, chomp-chomp, gobble-gobble, gulp. The hungry monster ate Otto from the ${userInputs[16]}. He went back to being a monster without a ${userInputs[17]}.</h2>`;
 
-let modifiedStory = `Once upon a time, in a land far away, there lived a nameless ${userInputs[0]}. The monster was dying to have a ${userInputs[1]}. So he decided to set out on a journey to find his ${userInputs[2]}.
-But the world was a large ${userInputs[3]}, so the monster split into two on his journey. One monster went to the ${userInputs[4]}. And the other went to the ${userInputs[5]}. The monster who went ${userInputs[6]} came to a village. "Mr. Blacksmith, please give me your ${userInputs[7]}."
-"You can't give someone a ${userInputs[8]}."
-"If you give me your ${userInputs[9]}, I'll jump into your stomach and make you ${userInputs[10]} in return."
-"Really? You'll make me ${userInputs[11]}? Okay, you can have my ${userInputs[12]}."
-And the monster went inside the blacksmith's ${userInputs[13]}.
-The monster became Otto the blacksmith. Otto was the ${userInputs[14]} man in town. But one day, "Look at me, look at me. The ${userInputs[15]} inside me has grown this large."
-Munch-munch, chomp-chomp, gobble-gobble, gulp. The hungry monster ate Otto from the ${userInputs[16]}. He went back to being a monster without a ${userInputs[17]}.`;
-
-
-
+alert("Your Awesome and Crazy Story is now ready ! I hope you are too 😂");
 
 
-console.log(modifiedStory == originalStory);
+
+
+console.log(originalStory);
+document.write(modifiedStory);
 
 //console.log(originalStory);
